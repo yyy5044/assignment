f69bbc0212d01a13e3b4b5af71e1233a1e809430
yangho <yyy5044@naver.com>
2019-10-17 21:09:13 +0900

    assignment04

---
+++ 20192513_04.c
@@ -0,0 +1,118 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <math.h>
+
+void signiture(fp) {
+	fputs("********************************************************\n", fp);
+	fputs("□■□□□■□□□■■□□□■□□□■□□□■■■■□□\n", fp);
+	fputs("□□■□■□□□■□□■□□■■□□■□□■□□□□■□\n", fp);
+	fputs("□□□■□□□□■□□■□□■□■□■□□■□□□□□□\n", fp);
+	fputs("□□□■□□□□■■■■□□■□■□■□□■□□■■■□\n", fp);
+	fputs("□□□■□□□□■□□■□□■□□■■□□■□□□■■□\n", fp);
+	fputs("□□□■□□□□■□□■□□■□□□■□□□■■■□■□\n", fp);
+	fputs("Oh Yangho\n", fp);
+	fputs("Student ID : 20192513\n", fp);
+	fputs("********************************************************\n", fp);
+}
+
+int main(int argc, char* argv[]) {
+
+	FILE* fp = NULL;
+	fp = fopen(argv[1], "w");
+
+	int any = argc - 2; //원소의 개수(프로그램 이름 뺀 것)
+	int num = pow(2.0, any); //2의 원소의 개수 제곱
+
+	int arr[10] = { 0, }; //최대 10개의 원소를 받을 수 있음
+	int binary; //십진법 - 이진법으로 바꾸기 위해 필요한 변수
+
+	int number = 2; //번호표를 출력하기 위한 변수
+	char emptyset = 155; //공집합의 ASCII
+
+	char str_num[64];
+
+	if (fp == NULL)
+	{
+		printf("파일 열기에 실패했습니다.\n");
+	}
+	else
+	{
+		printf("파일 열기에 성공했습니다.\n");
+
+		//프로그램
+		fputs("[0001] \u00f8\n", fp);
+
+		for (int j = num - 1; j > 0; j--) {
+
+			//십진법을 이진법으로 바꾸어서 arr 배열에 거꾸로 집어넣는 과정 (시프트연산 사용)
+			for (int i = 9; i >= 0; i--)
+			{
+				binary = num - j >> i;
+
+				if (binary & 1)
+				{
+					arr[9 - i] = 1;
+				}
+				else
+				{
+					arr[9 - i] = 0;
+				}
+
+			}
+
+			//원소의 개수를 세고 (원소의 개수 - 1)만큼의 콤마를 출력하기 위한 변수 두 개
+			int tmp = 0;
+			int count = 0;
+
+			//원소의 개수를 세는 과정
+			for (int i = 0; i < any; i++) {
+
+				if (arr[9 - i] == 1) {
+					tmp = tmp + 1;
+				}
+			}
+
+			//번호표를 출력하기 위한 과정
+
+			if (number < 10) {
+				sprintf(str_num,"[000%d] ", number);
+				fputs(str_num, fp);
+			}
+			if (number >= 10 && number < 100) {
+				sprintf(str_num,"[00%d] ", number);
+				fputs(str_num, fp);
+			}
+			if (number >= 100 && number < 1000) {
+				sprintf(str_num,"[0%d] ", number);
+				fputs(str_num, fp);
+			}
+			if (number >= 1000 && number < 10000) {
+				sprintf(str_num,"[%d] ", number);
+				fputs(str_num, fp);
+			}
+
+			//powerset 출력
+			for (int i = 0; i < any; i++) {
+
+				if (arr[9 - i] == 1) {
+					count = count + 1;
+					sprintf(str_num,"%s", argv[i + 2]); //powerset 출력
+					fputs(str_num, fp);
+
+					if (count < tmp) {
+						fputs(", ", fp); //원소의 개수보다 하나 적은 개수의 콤마를 출력
+					}
+				}
+
+				if (i == any - 1) {
+					fputs("\n", fp); //마지막 항에서 줄바꿈
+				}
+			}
+
+			number = number + 1;
+
+		}
+
+		signiture(fp);
+	}
+}
